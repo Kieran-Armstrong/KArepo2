f128ccad776cb3c5952606318041a00a88422a5e
Kieran <kieranarmstrong061@gmail.com>
2022-03-10 15:46:15 +0000

    KA added .stringlower for efficiency

--- Apps/replaceChar.py
+++ Apps/replaceChar.py
@@ -2,13 +2,11 @@
 from hashlib import new
 
 
-def charReplace(string):      
-    newString = string.replace("i", "1") 
-    newString = newString.replace("I", "1")
+def charReplace(string): 
+    string = string.lower()     
+    newString = string.replace("i", "1")
     newString = newString.replace("e", "3")
-    newString = newString.replace("E", "3")
     newString = newString.replace("a", "4")
-    newString = newString.replace("A", "4")
     
     
     print(newString)

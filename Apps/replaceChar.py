d27280667758bd423c03070fbe19a3ea2cefa45e
Kieran <kieranarmstrong061@gmail.com>
2022-03-10 15:49:53 +0000

    KA added all replacements

--- Apps/replaceChar.py
+++ Apps/replaceChar.py
@@ -7,11 +7,15 @@ def charReplace(string):
     newString = string.replace("i", "1")
     newString = newString.replace("e", "3")
     newString = newString.replace("a", "4")
+    newString = newString.replace("s", "5")
+    newString = newString.replace("b", "6")
+    newString = newString.replace("t", "7")
+    newString = newString.replace("o", "0")
     
     
     print(newString)
 
-string = input("Enter text")
+string = input("Enter text ")
 
 charReplace(string)
 

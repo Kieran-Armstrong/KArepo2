f730dccdc101adad98122f836ce55d2b7b3a8ddb
Kieran <kieranarmstrong061@gmail.com>
2022-03-11 21:34:58 +0000

    KA added code for qrGen

---
+++ Apps/qrGen/qrGen.py
@@ -0,0 +1,12 @@
+import pyqrcode
+from pyqrcode import QRCode
+
+s = input("Please enter a site URL ")
+
+qrName = input("What would you like to save the QR code as? (Just type the file name, we will add the .svg extension) ")
+
+url = pyqrcode.create(s)
+
+url.svg((qrName + ".svg"), scale = 8)
+
+

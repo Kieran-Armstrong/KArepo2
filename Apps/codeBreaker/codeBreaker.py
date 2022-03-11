a52189e673d5120b5e9731cccfc9a0ebbca41156
Kieran <kieranarmstrong061@gmail.com>
2022-03-11 01:10:41 +0000

    KA added decrypt function

--- Apps/codeBreaker/codeBreaker.py
+++ Apps/codeBreaker/codeBreaker.py
@@ -2,29 +2,57 @@ print("Please see codeBreaker.txt before starting for more info")
 
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-def encrypt(plainText, secretKey):
-    cipherText = ""
+def encrypt(plainText, encrpytKey):
+    encryptText = ""
     for character in plainText:
         if character.isalpha():
             character.lower()
             charIndex = alphabet.index(character)
-            newIndex = (charIndex + secretKey) % 26
+            newIndex = (charIndex + encrpytKey) % 26
             cipherChar = alphabet[newIndex].upper()
         else:
             cipherChar = character
         
-        cipherText += cipherChar
+        encryptText += cipherChar
 
-    print("Your cipher text is " + cipherText)
+    print("Your cipher text is " + encryptText)
 
 plainText = input("Enter a message to be encrypted: ")
 
 while True:
     try:
-        secretKey = int(input("Now enter how many characters in the alphabet you would like to shift: "))
+        encryptKey = int(input("Now enter how many characters in the alphabet you would like to shift: "))
     except ValueError:
         print("Please enter a whole number")
     else:
         break
 
-encrypt(plainText, secretKey)
+encrypt(plainText, encryptKey)
+
+def decrypt(decryptText, decryptKey):
+    alphaText = ""
+    for character in decryptText:
+        if character.isalpha():
+            character = character.lower()
+            charIndex = alphabet.index(character)
+            newIndex = (charIndex - decryptKey) % 26
+            alphaChar = alphabet[newIndex]
+        else:
+            alphaChar = character
+
+        alphaText += alphaChar
+
+    print("Your plaintext is: " + alphaText)
+
+decryptText = input("Please input a message to be decrypted: ")
+
+while True:
+    try:
+        decryptKey = int(input("Now enter how many characters in the alphabet you would like to shift back: "))
+    except ValueError:
+        print("Please enter a whole number")
+    else:
+        break
+
+decrypt(decryptText, decryptKey)
+
